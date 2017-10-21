c633b031bf57b13523cc1532941f711b711c7fab
leonzdev <leonz.dev@gmail.com>
2017-10-21 01:30:13 -0700

    Allow build.sh to run from any directory

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -vx
-rm -rf ./build
-mkdir -p ./build
-cd build
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "$SCRIPT")
+BUILD_DIR=$DIR/build
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 cmake .. -DWITH_LIBCPUID=OFF -DWITH_AEON=OFF -DWITH_HTTPD=OFF
 make
 
